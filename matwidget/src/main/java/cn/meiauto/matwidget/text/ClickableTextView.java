750f17c24eb99d99d1dc5db52e5b3102e40e0966
LiYang <yang.li@meiauto.cn>
2018-02-05 18:27:49 +0800

    Add ClickableTextView

---
+++ matwidget/src/main/java/cn/meiauto/matwidget/text/ClickableTextView.java
@@ -0,0 +1,124 @@
+package cn.meiauto.matwidget.text;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import cn.meiauto.matwidget.R;
+
+/**
+ * Text with clicking effect:
+ * text & image
+ * <pre>
+ * <cn.meiauto.matwidget.text.ClickableTextView
+ *     android:layout_width="wrap_content"
+ *     android:layout_height="wrap_content"
+ *     android:layout_gravity="center_horizontal"
+ *     android:drawablePadding="4dp"
+ *     android:drawableRight="@mipmap/ic_launcher"
+ *     android:gravity="center"
+ *     app:clickAlpha="0.1"
+ *     android:clickable="true"
+ *     android:text="XXX" />
+ * </pre>
+ * author : LiYang
+ * email  : yang.li@nx-engine.com
+ * time   : 2018/2/5
+ */
+public class ClickableTextView extends AppCompatTextView {
+
+    private final int mTextViewNormalColor, mTextViewPressedColor;
+    private final int mClickAlpha;
+
+    private Drawable[] mDrawables;
+    private boolean mClickable;
+
+    public ClickableTextView(Context context) {
+        this(context, null);
+    }
+
+    public ClickableTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ClickableTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.ClickTextView);
+        float ca = array.getFloat(R.styleable.ClickTextView_clickAlpha, 0.5F);
+        array.recycle();
+
+        if (ca < 0 && ca > 1) {
+            ca = 0.5F;
+        }
+        mClickAlpha = (int) (ca * 255);
+
+        mTextViewNormalColor = getCurrentTextColor();
+
+        int red = (mTextViewNormalColor & 0x00ff0000) >> 16;
+        int green = (mTextViewNormalColor & 0x0000ff00) >> 8;
+        int blue = (mTextViewNormalColor & 0x000000ff);
+        mTextViewPressedColor = Color.argb(mClickAlpha, red, green, blue);
+
+        mDrawables = getCompoundDrawables();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (mClickable) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    setClickStatus();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    resetClickStatus();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    performClick();
+                    resetClickStatus();
+                    break;
+                case MotionEvent.ACTION_CANCEL:
+                    resetClickStatus();
+                    break;
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void setClickStatus() {
+        setTextColor(mTextViewPressedColor);
+
+        for (int i = 0; i < 4; i++) {
+            Drawable drawable = mDrawables[i];
+            if (drawable != null) {
+                drawable.setAlpha(mClickAlpha);
+            }
+        }
+    }
+
+    private void resetClickStatus() {
+        setTextColor(mTextViewNormalColor);
+
+        for (int i = 0; i < 4; i++) {
+            Drawable drawable = mDrawables[i];
+            if (drawable != null) {
+                drawable.setAlpha(255);
+            }
+        }
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+    @Override
+    public void setClickable(boolean clickable) {
+        super.setClickable(clickable);
+        mClickable = clickable;
+    }
+}
