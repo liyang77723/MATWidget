cb6e1794ba8350f0ee33e952b5e5ba1fb463a981
LiYang <yang.li@meiauto.cn>
2018-03-01 11:07:53 +0800

    finished MarqueeTextView

--- matwidget/src/main/java/cn/meiauto/matwidget/text/MarqueeTextView.java
+++ matwidget/src/main/java/cn/meiauto/matwidget/text/MarqueeTextView.java
@@ -1,50 +1,73 @@
 package cn.meiauto.matwidget.text;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Rect;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatTextView;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 
 /**
- * 跑马灯TextView
+ * 跑马灯效果的TextView, 使用方式：
+ * 启动/关闭：{@link #setMarqueeEnable(boolean)}
+ * xml文件中记得设置：android:focusable="true", android:singleLine="true"
+ * 外层包裹布局避免闪烁
+ * <p>
+ * <FrameLayout
+ * android:layout_width="match_parent"
+ * android:layout_height="wrap_content">
  * <cn.meiauto.matwidget.text.MarqueeTextView
+ * style="@style/MATWidget.MarqueeTextStyle"
  * android:layout_width="match_parent"
  * android:layout_height="wrap_content"
- * android:ellipsize="marquee"
- * android:focusable="true"
- * android:focusableInTouchMode="true"
- * android:marqueeRepeatLimit="marquee_forever"
- * android:scrollHorizontally="true"
- * android:singleLine="true"
- * android:text="run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run" />
+ * android:text="xxx"
+ * </FrameLayout>
  */
+
 public class MarqueeTextView extends AppCompatTextView {
+
+    private boolean isMarqueeEnable = true;
+
     public MarqueeTextView(Context context) {
         super(context);
     }
 
-    public MarqueeTextView(Context context, AttributeSet attrs) {
+    public MarqueeTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public MarqueeTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public MarqueeTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
-    //重写isFocused方法，让其一直返回true  
-    public boolean isFocused() {
-        return true;
+    public void setMarqueeEnable(boolean enable) {
+        if (isMarqueeEnable != enable) {
+            isMarqueeEnable = enable;
+            if (enable) {
+                setEllipsize(TextUtils.TruncateAt.MARQUEE);
+            } else {
+                setEllipsize(TextUtils.TruncateAt.END);
+            }
+            onWindowFocusChanged(enable);
+        }
     }
 
-    @SuppressLint("MissingSuperCall")
+    public boolean isMarqueeEnable() {
+        return isMarqueeEnable;
+    }
+
+    @Override
+    public boolean isFocused() {
+        return isMarqueeEnable;
+    }
+
     @Override
     protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
-
+        super.onFocusChanged(isMarqueeEnable, direction, previouslyFocusedRect);
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasWindowFocus) {
-
+        super.onWindowFocusChanged(isMarqueeEnable);
     }
-}  
+}
