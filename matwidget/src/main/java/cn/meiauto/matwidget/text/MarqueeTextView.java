f92e773a347712d16ddedc09d8f2b06a84898fa0
LiYang <yang.li@meiauto.cn>
2018-03-06 15:08:24 +0800

    changed MarqueeTextView

--- matwidget/src/main/java/cn/meiauto/matwidget/text/MarqueeTextView.java
+++ matwidget/src/main/java/cn/meiauto/matwidget/text/MarqueeTextView.java
@@ -1,73 +1,41 @@
 package cn.meiauto.matwidget.text;
 
 import android.content.Context;
-import android.graphics.Rect;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.AppCompatTextView;
-import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.view.ContextThemeWrapper;
+
+import cn.meiauto.matwidget.R;
 
 /**
- * 跑马灯效果的TextView, 使用方式：
- * 启动/关闭：{@link #setMarqueeEnable(boolean)}
- * xml文件中记得设置：android:focusable="true", android:singleLine="true"
+ * 跑马灯效果的TextView
  * 外层包裹布局避免闪烁
  * <p>
  * <FrameLayout
  * android:layout_width="match_parent"
  * android:layout_height="wrap_content">
  * <cn.meiauto.matwidget.text.MarqueeTextView
- * style="@style/MATWidget.MarqueeTextStyle"
  * android:layout_width="match_parent"
  * android:layout_height="wrap_content"
- * android:text="xxx"
+ * android:text="run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run run"
+ * tools:style="@style/MATWidget.MarqueeTextStyle" />
  * </FrameLayout>
+ * <p>
+ * author : LiYang
+ * email  : yang.li@nx-engine.com
+ * time   : 2018/3/6
  */
-
-public class MarqueeTextView extends AppCompatTextView {
-
-    private boolean isMarqueeEnable = true;
-
+public class MarqueeTextView extends android.support.v7.widget.AppCompatTextView {
     public MarqueeTextView(Context context) {
-        super(context);
+        this(new ContextThemeWrapper(context, R.style.MATWidget_MarqueeTextStyle), null, 0);
     }
 
-    public MarqueeTextView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+    public MarqueeTextView(Context context, AttributeSet attrs) {
+        this(new ContextThemeWrapper(context, R.style.MATWidget_MarqueeTextStyle), attrs, 0);
     }
 
-    public MarqueeTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+    public MarqueeTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(new ContextThemeWrapper(context, R.style.MATWidget_MarqueeTextStyle), attrs, defStyleAttr);
 
-    public void setMarqueeEnable(boolean enable) {
-        if (isMarqueeEnable != enable) {
-            isMarqueeEnable = enable;
-            if (enable) {
-                setEllipsize(TextUtils.TruncateAt.MARQUEE);
-            } else {
-                setEllipsize(TextUtils.TruncateAt.END);
-            }
-            onWindowFocusChanged(enable);
-        }
-    }
-
-    public boolean isMarqueeEnable() {
-        return isMarqueeEnable;
-    }
-
-    @Override
-    public boolean isFocused() {
-        return isMarqueeEnable;
-    }
-
-    @Override
-    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
-        super.onFocusChanged(isMarqueeEnable, direction, previouslyFocusedRect);
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasWindowFocus) {
-        super.onWindowFocusChanged(isMarqueeEnable);
+        setSelected(true);
     }
 }
